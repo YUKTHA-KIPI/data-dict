49afab22a98690357e65912603c471e0dd659281
YUKTHA-KIPI <104973913+YUKTHA-KIPI@users.noreply.github.com>
2023-02-07 14:20:26 +0530

    UPDATE

--- sign_in.py
+++ sign_in.py
@@ -49,7 +49,7 @@ def run_custom_code():
         height: 45px;
         background-color: #EEEEEE;
         color: #000000;
-        placeholder: "snowflake user name";
+        placeholder: "someone@kipi.com";
         border-radius: 5px;
         caret-color: #000000;
     }
@@ -100,7 +100,7 @@ def run_custom_code():
     }
     /*card*/
     div#authcardholder{
-        background-color: #FFFFFF; 
+        background-color: #1D1C21; 
         margin-left: auto;
         margin-right: auto;
         padding: 10px 30px 0 30px;
